0728b16dfdd0222a13c541a75f4707a721c0156c
Anders Conbere <aconbere@gmail.com>
2012-10-20 13:05:11 -0500

    update to use traits

--- src/main/scala/Markov.scala
+++ src/main/scala/Markov.scala
@@ -3,27 +3,109 @@ package com.conbere.markov
 import scala.util.Random
 import scala.io.Source
 
-trait MarkovChain[C,T] {
-  val chain: Map[List[C], T]
+package object M {
+  def random[K](l: List[K]) = l(new Random().nextInt(l.length))
+}
+
+trait Frequency[C] {
+  def add(v: C): Frequency[C]
+  def choice(): Option[C]
+  def contains(v: C): Boolean
+}
+
+class DirectFrequency[C](val frequencies: List[C])
+extends Frequency[C] {
+  def this() = this(List[C]())
+  def add(v: C) = new DirectFrequency(frequencies :+ v)
+  def contains(v: C) = frequencies.contains(v)
+  def choice() = Some(M.random(frequencies))
+}
+
+class RouletteFrequency[C](val frequencies: Map[C,Int])
+extends Frequency[C] {
+  def this() = this(Map[C,Int]())
+
+  def add(v: C): RouletteFrequency[C] =
+    new RouletteFrequency(frequencies + (v -> (frequencies.getOrElse(v,0) + 1)))
+
+  def contains(v: C) = frequencies.contains(v)
+
+  def choice() = {
+    def inner(l: List[(C,Int)], n: Int): Option[C]= {
+      if (l.isEmpty) {
+        None
+      } else {
+        l.head match {
+          case head @ (c, i) =>
+            if(l.length < 1 || (n - i) <= 0)
+              Some(c)
+            else
+              inner(l.tail, n - i)
+        }
+      }
+    }
+    inner(frequencies.toList,
+          new Random()
+            .nextInt(frequencies.values.sum))
+  }
+}
+
+trait EdgeFrequency[T, C] {
+  val frequencies: Map[T,RouletteFrequency[C]]
+
+  def add(k: T, v: C): EdgeFrequency[T,C]
+
+  def contains(k: T) = frequencies.contains(k)
+  def keys = frequencies.keys
+  def get(k: T): Option[RouletteFrequency[C]] =
+    frequencies.get(k)
+}
+
+class ListEdgeFrequency[C] (val frequencies: Map[List[C], RouletteFrequency[C]])
+extends EdgeFrequency[List[C], C] {
+  def this() = this(Map[List[C], RouletteFrequency[C]]())
+
+  def add(k:List[C], v:C) =
+    new ListEdgeFrequency[C](frequencies +
+      (k -> frequencies.getOrElse(k, new RouletteFrequency()).add(v)))
+}
+
+class TupleEdgeFrequency[C] (val frequencies: Map[(C,C), RouletteFrequency[C]])
+extends EdgeFrequency[(C,C), C] {
+  def this() = this(Map[(C,C), RouletteFrequency[C]]())
+
+  def add(k:(C,C), v:C) =
+    new TupleEdgeFrequency[C](frequencies +
+      (k -> frequencies.getOrElse(k, new RouletteFrequency()).add(v)))
+}
+
+trait Chainable[T, C] {
+  val edges: EdgeFrequency[T, C]
+
   val start: C
   val stop: C
   val chainLength: Int
 
-  def add(k: List[C], v: C): MarkovChain[C,T]
-  def choice(x: T): Option[C]
+  def insert(word: T)
+  def seed: T
+  def generate(maxLength: Int): List[C]
 
-  def contains(k: List[C]) = chain.contains(k)
-  def random[K](l: List[K]) = l(new Random().nextInt(l.length))
-
-  def seed = random(chain.keys.filter(x => x.head == start).toList)
-
-  def next(seed: List[C]): Option[C] = {
-    for (l <- chain.get(seed);
-         n <- choice(l))
+  def next(seed: T): Option[C] = {
+    for (l <- edges.get(seed);
+         n <- l.choice)
     yield {
        n
     }
   }
+}
+
+
+class MarkovChain[C](val start: C,
+                     val stop: C,
+                     val chainLength: Int,
+                     val edges: EdgeFrequency[List[C],C])
+extends Chainable[List[C], C] {
+  def seed = M.random(edges.keys.filter(x => x.head == start).toList)
 
   def insert(word: List[C]) = {
     val w = (start :: word) :+ stop
@@ -31,7 +113,10 @@ trait MarkovChain[C,T] {
     w.sliding(chainLength + 1)
      .foldLeft(this)((acc, l) =>
        if (l.length == chainLength + 1)
-         acc.add(l.slice(0, chainLength), l.last)
+         new MarkovChain[C](start,
+                            stop,
+                            chainLength,
+                            edges.add(l.slice(0, chainLength), l.last))
        else
          acc
      )
@@ -54,55 +139,5 @@ trait MarkovChain[C,T] {
     }
     val s = seed
     inner(maxLength, s.tail, s)
-  }
-}
-
-class MarkovChainList[C](val start: C,
-                         val stop: C,
-                         val chainLength: Int,
-                         val chain: Map[List[C], List[C]])
-extends MarkovChain[C, List[C]] {
-  def this(start: C, stop: C, chainLength: Int) =
-    this(start, stop, chainLength, Map[List[C], List[C]]())
-
-  def add(k: List[C], v: C) = {
-    val value = chain.getOrElse(k, List()) :+ v
-    val newChain = chain + (k -> value)
-    new MarkovChainList[C](start, stop, chainLength, chain + (k -> value))
-  }
-
-  def choice(l: List[C]): Option[C] = Some(random(l))
-}
-
-class MarkovChainMap[C](val start: C,
-             val stop: C,
-             val chainLength: Int,
-             val chain: Map[List[C], Map[C,Int]])
-extends MarkovChain[C, Map[C, Int]] {
-  def this(start: C, stop: C, chainLength: Int) =
-    this(start, stop, chainLength, Map[List[C], Map[C,Int]]())
-
-  def add(k: List[C], v: C) = {
-    val m = chain.getOrElse(k, Map[C,Int]())
-    val newM = m + (v -> (m.getOrElse(v, 0) + 1))
-    new MarkovChainMap[C](start, stop, chainLength, chain + (k -> newM))
-  }
-
-  def choice(m: Map[C,Int]): Option[C] = {
-    def inner(l: List[(C,Int)], c: Int): Option[C]= {
-      l match  {
-        case (char, i) :: Nil =>
-          Some(char)
-        case (char, i) :: xs =>
-          val n = (c - i)
-          if (n <= 0)
-            Some(char)
-          else
-            inner(l.tail, n)
-        case Nil =>
-          None
-      }
-    }
-    inner(m.toList, new Random().nextInt(m.values.sum))
   }
 }
